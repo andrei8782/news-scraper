57e99f0a20fe9bfb25a98d585a0eb529ab8dc30d
Andrei Stefan <14292505+andrei8782@users.noreply.github.com>
2022-03-21 18:34:29 +0000

    Refactored into class

--- links_cryptoglobe.py
+++ links_cryptoglobe.py
@@ -9,67 +9,62 @@ from selenium.common.exceptions import StaleElementReferenceException
 import time
 from random import randrange
 import requests
+import sys
 
-def get_links(browser):
-    page = browser.page_source
-    soup = BeautifulSoup(page, "html.parser")
+class LinkScraper:
+    def __init__(self, url):
+        chrome_options = Options() 
+        # chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser.get(url)
 
-    list = soup.find_all("div", class_="item-news ng-scope")
 
-    n = 0
-    with open("output/cryptoglobe/links", "w") as file:
-        for div in list:
-            heading = div.find("h3")
-            link = heading.find("a", class_="ng-binding")
-            if link['href'] != '':
-                file.write(link['href'] + "\n")
+
+    def get_links(self, out_file):
+        page = self.browser.page_source
+        soup = BeautifulSoup(page, "html.parser")
+
+        list = soup.find_all("div", class_="item-news ng-scope")
+
+        n = 0
+        # out_file = "output/cryptoglobe/sample_links"
+        # tmp_file = "output/cryptoglobe/tmp_links"
+        with open(out_file, "w") as file:
+            for div in list:
+                heading = div.find("h3")
+                link = heading.find("a", class_="ng-binding")['href']
+                if link == '':
+                    continue
+                file.write(link + "\n")
                 n += 1
-    print("{} links have been saved.".format(n))
+        print("{} links have been saved.".format(n))
 
-def scroll_down(browser):
-    last_height = browser.execute_script("return document.body.scrollHeight")
+    def scroll_down(self, out_file):
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
 
-    page = 1
-    print("Page {} loaded.".format(page))
-
-    while True:
-        if page % 10 == 0:
-            get_links(browser)
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        page += 1
+        page = 1
         print("Page {} loaded.".format(page))
-        time.sleep(2)
 
-        new_height = browser.execute_script("return document.body.scrollHeight")
+        while True:
+            if page % 5 == 0:
+                self.get_links(out_file)
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            page += 1
+            print("Page {} loaded.".format(page))
+            time.sleep(2)
 
-        if new_height == last_height:
-            break
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
 
-        last_height = new_height
-    
-    return browser
+            if new_height == last_height:
+                break
 
-def load_browser(url, save_page=False, load_from_memory=False):
-    if load_from_memory == True:
-        with open('output/tmp_loaded_page', 'r') as f:
-            return f.read()
-
-    chrome_options = Options() 
-    chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_argument("--headless")
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get(url)
-
-    if save_page == True:
-        loaded_html = browser.page_source
-        with open('output/tmp_loaded_page', 'w') as f:
-            f.write(loaded_html)
-
-    return browser
+            last_height = new_height
+        
+        return self.browser
 
 if __name__ == "__main__":
     url = "https://www.cryptocompare.com/news/list/latest/?feeds=cryptoglobe"
-
-    browser = load_browser(url)
-    browser = scroll_down(browser)
-    get_links(browser)
+    link_scraper = LinkScraper(url)
+    link_scraper.scroll_down(out_file="output/cryptoglobe/tmp_links")
+    link_scraper.get_links(out_file="output/cryptoglobe/tmp_links")
